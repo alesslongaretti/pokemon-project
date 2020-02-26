87363deda8e9b82f3ebc8266b14ef4448a582ff7
tauan-longaretti <tauanlongaretti@gmail.com>
2020-02-25 20:24:25 -0500

    imported icons for each pokemon type and added them to a new Dashboard component

---
+++ pokemon/src/components/Dashboard.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import Bug from '../Images/Bug.png';
+import Dark from '../Images/Dark.png';
+import Dragon from '../Images/Dragon.png';
+import Electric from '../Images/Electric.png';
+import Fairy from '../Images/Fairy.png';
+import Fighting from '../Images/Fighting.png';
+import Fire from '../Images/Fire.png';
+import Flying from '../Images/Flying.png';
+import Ghost from '../Images/Ghost.png';
+import Grass from '../Images/Grass.png';
+import Ground from '../Images/Ground.png';
+import Ice from '../Images/Ice.png';
+import Normal from '../Images/Normal.png';
+import Poison from '../Images/Poison.png';
+import Psychic from '../Images/Psychic.png';
+import Rock from '../Images/Rock.png';
+import Steel from '../Images/Steel.png';
+import Water from '../Images/Water.png';
+
+const Dashboard = () => {
+    return (
+        <div className="dashboard">
+            <img src={Bug} alt="bug type icon" />
+            <img src={Dark} alt="dark type icon" />
+            <img src={Dragon} alt="dragon type icon" />
+            <img src={Electric} alt="electric type icon" />
+            <img src={Fairy} alt="fairy type icon" />
+            <img src={Fighting} alt="fighting type icon" />
+            <img src={Fire} alt="fire type icon" />
+            <img src={Flying} alt="flying type icon" />
+            <img src={Ghost} alt="ghost type icon" />
+            <img src={Grass} alt="grass type icon" />
+            <img src={Ground} alt="ground type icon" />
+            <img src={Ice} alt="ice type icon" />
+            <img src={Normal} alt="normal type icon" />
+            <img src={Poison} alt="poison type icon" />
+            <img src={Psychic} alt="psychic type icon" />
+            <img src={Rock} alt="rock type icon" />
+            <img src={Steel} alt="steel type icon" />
+            <img src={Water} alt="water type icon" />
+        </div>
+    )
+}
+
+export default Dashboard;
