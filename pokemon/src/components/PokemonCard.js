d4e9a7c752572c09b8574edc1cbb6ac1f4c48097
tauan-longaretti <tauanlongaretti@gmail.com>
2020-02-27 04:16:48 -0500

    Created Pokemon Card Component that will be used to display each pokemon

---
+++ pokemon/src/components/PokemonCard.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledDiv = styled.div`
+    text-align: center;
+    background-color: #FFCB05;
+    padding: 1%;
+    margin: 1%;
+    width: 25%;
+    border-radius: 8px;
+`;
+
+const StyledImg = styled.img`
+    margin: 1%;
+`;
+
+const StyledHeader = styled.h3`
+    margin: 1%;
+    color: #003A70;
+`;
+
+const StyledParagraph = styled.p`
+    margin: .5%;
+    color: #3D7DCA;
+`;
+
+const PokemonCard = () => {
+    return (
+        <StyledDiv>
+            <StyledImg src={response.data.sprites.font_default} />
+            <StyledHeader>{pokemon.name}</StyledHeader>
+            <StyledParagraph>Height: {response.data.height}</StyledParagraph>
+            <StyledParagraph>Weight: {response.data.weight}</StyledParagraph>
+        </StyledDiv>
+    )
+}
+
+export default PokemonCard;
