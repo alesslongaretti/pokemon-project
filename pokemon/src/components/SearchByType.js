1243c288a1cd74d4c1943444aad7219fa89b39f3
alessandra <alesslongaretti@gmail.com>
2020-03-16 02:07:27 -0400

    added name to each icon and styled title

--- pokemon/src/components/SearchByType.js
+++ pokemon/src/components/SearchByType.js
@@ -32,6 +32,9 @@ const StyledLinksDiv = styled.div`
 
 const StyledLink = styled(Link)`
   margin: 3%;
+  display: block;
+  text-decoration: none;
+  color: black;
 `;
 
 const StyledIcon = styled.img`
@@ -41,62 +44,139 @@ const StyledIcon = styled.img`
   box-shadow: 10px 10px 30px 0px rgba(0,0,0,0.9);
 `;
 
+const StyledDivName = styled.div`
+
+`;
+
 const SearchByType = () => {
   return (
     <StyledDiv className="headers">
-      <h2>Search Pokemons By Type:</h2>
+      <a href="https://fontmeme.com/pokemon-font/">
+        <img src="https://fontmeme.com/permalink/200316/752d21e33c2682ba029d3ef3f87fd818.png" alt="pokemon-font" border="0"/>
+        </a>
       <StyledLinksDiv>
+
         <StyledLink className="link" to="/bug-type">
-          <StyledIcon src={Bug} alt="bug type icon" />
+          <StyledDivName>
+            <h2>Bug</h2>
+        </StyledDivName>
+          <div>
+            <StyledIcon src={Bug} alt="bug type icon" />
+          </div>
         </StyledLink>
+
         <StyledLink className="link" to="/dragon-type">
+          <div>
+          <h2> Dragon </h2>
+          </div>
           <StyledIcon src={Dragon} alt="dragon type icon" />
         </StyledLink>
+
         <StyledLink className="link" to="/electric-type">
+          <div>
+            <h2> Electric </h2>
+          </div>
           <StyledIcon src={Electric} alt="electric type icon" />
         </StyledLink>
+
         <StyledLink className="link" to="/fairy-type">
+          <div>
+            <h2> Fairy </h2>
+          </div>
           <StyledIcon src={Fairy} alt="fairy type icon" />
         </StyledLink>
+
         <StyledLink className="link" to="/fighting-type">
+          <div>
+            <h2> fighting</h2>
+          </div>
           <StyledIcon src={Fighting} alt="fighting type icon" />
         </StyledLink>
+
         <StyledLink className="link" to="/fire-type">
+          <div>
+            <h2> Fire </h2>
+          </div>
           <StyledIcon src={Fire} alt="fire type icon" />
         </StyledLink>
+
         <StyledLink className="link" to="/flying-type">
+          <div>
+            <h2> Flying </h2>
+          </div>
           <StyledIcon src={Flying} alt="flying type icon" />
         </StyledLink>
+
         <StyledLink className="link" to="/ghost-type">
+          <div>
+            <h2> Ghost </h2>
+          </div>
           <StyledIcon src={Ghost} alt="ghost type icon" />
         </StyledLink>
+
         <StyledLink className="link" to="/grass-type">
+          <div>
+            <h2> Grass </h2>
+          </div>
           <StyledIcon src={Grass} alt="grass type icon" />
         </StyledLink>
+
         <StyledLink className="link" to="/ground-type">
+          <div>
+            <h2> Ground </h2>
+          </div>
           <StyledIcon src={Ground} alt="ground type icon" />
         </StyledLink>
+
         <StyledLink className="link" to="/ice-type">
+          <div>
+            <h2> Ice </h2>
+          </div>
           <StyledIcon src={Ice} alt="Ice type icon" />
         </StyledLink>
+
         <StyledLink className="link" to="/normal-type">
+          <div>
+            <h2> Normal </h2>
+          </div>
           <StyledIcon src={Normal} alt="normal type icon" />
         </StyledLink>
+
         <StyledLink className="link" to="/poison-type">
+          <div>
+            <h2> Poison </h2>
+          </div>
           <StyledIcon src={Poison} alt="poison type icon" />
         </StyledLink>
+
         <StyledLink className="link" to="/psychic-type">
+          <div>
+            <h2> Psychic </h2>
+          </div>
           <StyledIcon src={Psychic} alt="psychic type icon" />
         </StyledLink>
+
         <StyledLink className="link" to="/rock-type">
+          <div>
+            <h2> Rock </h2>
+          </div>
           <StyledIcon src={Rock} alt="rock type icon" />
         </StyledLink>
+
         <StyledLink className="link" to="/steel-type">
+          <div>
+            <h2> Steel </h2>
+          </div>
           <StyledIcon src={Steel} alt="steel type icon" />
         </StyledLink>
+
         <StyledLink className="link" to="/water-type">
+          <div>
+            <h2> Water </h2>
+          </div>
           <StyledIcon src={Water} alt="water type icon" />
         </StyledLink>
+
       </StyledLinksDiv>
     </StyledDiv>
   );
